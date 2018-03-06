bfa3615c67639b94967e8352426e6b2db490113b
Sean Fuller <seanbfuller@localhost>
2018-03-05 22:09:46 -0600

    Added start of routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RollsComponent } from './rolls/rolls.component';
 import { RollsService } from './rolls/rolls.service';
+import { NotFoundComponent } from './not-found.component';
+
+const appRoutes: Routes = [
+  // hit rolls
+  { path: 'hit-rolls', component: RollsComponent },
+  // default
+  { path: '',
+    redirectTo: '/hit-rolls',
+    pathMatch: 'full'
+  },
+  // 404
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RollsComponent
+    RollsComponent,
+    NotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {
+        // for debugging purposes only
+        enableTracing: true 
+      }
+    ),
     BrowserModule,
     FormsModule
   ],
   providers: [RollsService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
